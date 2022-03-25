d4773c807cc9f944b8089997ba2db2ba8466e3e9
Rimas-S <96993500+Rimas-S@users.noreply.github.com>
2022-03-25 20:38:55 +0100

    update product list

--- client/src/components/Cards/ProductCard/ProductCard.tsx
+++ client/src/components/Cards/ProductCard/ProductCard.tsx
@@ -11,24 +11,26 @@ type ProductCardProps = {
 const ProductCard = ({ images, price, title }: ProductCardProps) => {
   return (
     <div className="product-card">
-      <div className="product-card__image">
-        <MyCarousel images={images} />
-      </div>
+      <div className="flex">
+        <div className="product-card__image">
+          <MyCarousel images={images} />
+        </div>
 
-      <h3 className="product-card__price">
-        <span>&#8364;</span>
-        {price}
-      </h3>
+        <h3 className="product-card__price">
+          <span>&#8364;</span>
+          {price}
+        </h3>
 
-      <h3 className="product-card__title">{title}</h3>
+        <h3 className="product-card__title">{title}</h3>
 
-      <div className="product-card__buttons">
-        <Button color="secondary" variant="outlined">
-          Detail
-        </Button>
-        <Button color="success" variant="outlined">
-          Buy
-        </Button>
+        <div className="product-card__buttons">
+          <Button color="secondary" variant="outlined">
+            Detail
+          </Button>
+          <Button color="success" variant="outlined">
+            Buy
+          </Button>
+        </div>
       </div>
     </div>
   );
